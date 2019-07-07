94719d4c10573c0721b1a2d71116aba9e255ecd4
saharshleo <saharshjain2000@gmail.com>
2019-07-07 14:09:00 +0530

    scrollview for viewing post accessing title,date and description for post is remaining

--- app/src/main/java/com/example/inheritance/ViewPost.java
+++ app/src/main/java/com/example/inheritance/ViewPost.java
@@ -3,6 +3,8 @@ package com.example.inheritance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ViewPost extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ViewPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_post);
+        TextView vpPostTitle = (TextView) findViewById(R.id.tvviewpost_title);
+        TextView vpPostDate = (TextView) findViewById(R.id.tvviewpost_date);
+        TextView vpPostDescription = (TextView) findViewById(R.id.tvviewpost_description);
+        ImageView vpImage = (ImageView) findViewById(R.id.ivviewpost_image);
+  //      vpPostDate.setText(date);
+
+
     }
 }
