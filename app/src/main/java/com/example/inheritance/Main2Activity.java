01d48417a9192724a75db3d5fae47d424ee7f976
saharshleo <saharshjain2000@gmail.com>
2019-07-09 00:34:39 +0530

    work on viewpost hardcoded id and committee

--- app/src/main/java/com/example/inheritance/Main2Activity.java
+++ app/src/main/java/com/example/inheritance/Main2Activity.java
@@ -1,5 +1,5 @@
 package com.example.inheritance;
-
+// test file
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,20 +14,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ViewPost extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_post);
+        setContentView(R.layout.activity_main2);
         final TextView vpPostTitle = (TextView) findViewById(R.id.tvviewpost_title);
         final TextView vpPostDate = (TextView) findViewById(R.id.tvviewpost_date);
         final TextView vpPostDescription = (TextView) findViewById(R.id.tvviewpost_description);
-   //     ImageView vpImage = (ImageView) findViewById(R.id.ivviewpost_image);
-  //      vpPostDate.setText(date);
-        Intent intent = getIntent();
-        String postID = intent.getStringExtra("postID");
-        String committee = intent.getStringExtra("committee");
+        //     ImageView vpImage = (ImageView) findViewById(R.id.ivviewpost_image);
+        //      vpPostDate.setText(date);
+       // Intent intent = getIntent();
+       // String postID = intent.getStringExtra("postID");
+        //String committee = intent.getStringExtra("committee");
+        String postID = "-Lj0wApIMkB4B9HBRxdI";
+        String committee = "AeroVJTI";
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference(committee).child(postID);
         dbRef.keepSynced(true);
         dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -39,8 +41,8 @@ public class ViewPost extends AppCompatActivity {
                     vpPostDate.setText(post.getDate());
                     vpPostDescription.setText(post.getDescription());
 
-
                 }
+
             }
 
             @Override
